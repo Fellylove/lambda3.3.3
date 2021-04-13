34db67e24b8b5d8d1bfdd57dc8bf71393b31c6ad
Felicity Burris <sylviaf_ssanyu@yahoo.com>
2021-04-13 16:00:06 -0600

    assignment in progress

---
+++ env/static/tweet.js
@@ -0,0 +1,15 @@
+
+$('#author_text_input').innerText = "Please enter a twitter user's handle";
+$('#author_submit').click(function () {
+    $.ajax({
+        url: '/add_user?twitter_handle=' + $('#author_text_input').val(),
+        type: 'GET',
+    });
+});
+$('#tweet_text_input').innerText = "Please enter a tweet to classify";
+$('#tweet_submit').click(function () {
+    $.ajax({
+        url: '/predict_author?tweet_to_classify=' + $('#tweet_text_input').val(),
+        type: 'GET'
+    });
+});
